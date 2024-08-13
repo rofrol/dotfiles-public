e0e4ebb4bbd3baf714328ebe6d6c6b2b1e235995
Roman Frołow <rofrol@gmail.com>
2024-08-13 11:00:50 +0200

    vb"_d is just db
    if you wanted to delete to first non-space character, you can use `d^`.
    
    https://stackoverflow.com/questions/14238669/vi-to-delete-from-the-beginning-of-the-line-till-the-cursor/14238724#14238724

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -24,7 +24,7 @@ keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
+keymap.set("n", "dw", 'db')
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
